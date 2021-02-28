8017cae2ff90803a4bd449da34b2862a65fa4e76
Daniel Altman <44586460+darango401@users.noreply.github.com>
2021-02-28 13:24:18 -0500

    V 1.0

---
+++ js/main.js
@@ -0,0 +1,62 @@
+var Splash, Inicio, Mapa, Carta, Reserva_Sin_Login, Reserva, Login, Registro;
+var btn_carta,btn_reserva, btn_mapa, btn_registro, btn_login;
+var secciones = [];
+
+window.onload = () =>{
+    crearReferencias();
+    agregarEventos();
+    setTimeout(()=>{irA(Inicio);},1500);
+}
+function crearReferencias()
+{
+    Splash = document.getElementById("Splash");
+    Inicio = document.getElementById("Inicio");
+    Mapa = document.getElementById("Mapa");
+    Carta = document.getElementById("Carta");
+    Reserva_Sin_Login = document.getElementById("Reserva_Sin_Login");
+    Reserva = document.getElementById("Reserva");
+    Login = document.getElementById("Login");
+    Registro = document.getElementById("Registro");
+    secciones = [Splash, Inicio, Mapa, Carta, Reserva_Sin_Login, Reserva, Login, Registro];
+    btn_carta = document.getElementById("btn_carta");
+    btn_reserva = document.getElementById("btn_reserva");
+    btn_mapa = document.getElementById("btn_mapa");
+    btn_registro = document.getElementById("btn_registro");
+    btn_registrate = document.getElementById("btn_registrate");
+    btn_login = document.getElementById("btn_login");
+    btn_volver1 = document.getElementById("btn_volver1");
+    btn_volver2 = document.getElementById("btn_volver2");
+    btn_volver3 = document.getElementById("btn_volver3");
+    btn_volver4 = document.getElementById("btn_volver4");
+    btn_volver5 = document.getElementById("btn_volver5");
+    btn_volver6 = document.getElementById("btn_volver6");
+
+
+    ocultarSecciones();
+    Splash.classList.remove("ocultar")
+}
+function agregarEventos() {
+        btn_carta.addEventListener("click",()=>{irA(Carta);});
+        btn_reserva.addEventListener("click",()=>{irA(Reserva);});
+        btn_mapa.addEventListener("click",()=>{irA(Mapa);});
+        btn_registro.addEventListener("click",()=>{irA(Registro);});
+        btn_registrate.addEventListener("click",()=>{irA(Registro);});
+        btn_login.addEventListener("click",()=>{irA(Login);});
+        btn_volver1.addEventListener("click",()=>{irA(Inicio);});
+        btn_volver2.addEventListener("click",()=>{irA(Inicio);});
+        btn_volver3.addEventListener("click",()=>{irA(Inicio);});
+        btn_volver4.addEventListener("click",()=>{irA(Inicio);});
+        btn_volver5.addEventListener("click",()=>{irA(Inicio);});
+        btn_volver6.addEventListener("click",()=>{irA(Inicio);});
+}
+function irA(Seccion){
+    ocultarSecciones();
+    Seccion.classList.remove("ocultar")
+}
+function ocultarSecciones()
+{
+    for(i in secciones)
+    {
+        secciones[i].classList.add("ocultar");
+    }
+}
